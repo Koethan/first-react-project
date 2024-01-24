bb417af44d335aa0081b8abc554536f03b7f7ec8
Ethan Ospina <ethan_ospina@yahoo.com>
2024-01-23 21:45:20 -0500

    wrapped the whole game in a function, still having trouble restarting if a tie

--- newscript.js
+++ newscript.js
@@ -1,33 +1,67 @@
-function getComputerChoice(){
-    let random = Math.floor(Math.random() * 3)
-    if(random == 0){
-        return "rock"
-    }else if(random == 1){
-        return "paper"
-    }else if(random == 2){
-        return "scissors"
+
+
+
+// function game(computer,human){
+//     // let scoreHuman = 0
+//     // let scoreComputer = 0
+//     // for(let i = 0; i <= 4; i++){
+//     //     console.log(oneRound(computer, human))
+//     //     console.log(i)
+//     //     // if(oneRound(computer,human).includes('win')){
+//     //     //     scoreHuman++
+//     //     // }else if(oneRound(computer, human).includes('lose')){
+//     //     //     scoreComputer++
+//     //     // }
+//     // }
+//     (oneRound(computer,human))
+
+//     // return `The final score was computer: ${scoreComputer} to player: ${scoreHuman}!`
+// } 
+
+function game(){
+
+    function getComputerChoice(){
+        let random = Math.floor(Math.random() * 3)
+        if(random == 0){
+            return "rock"
+        }else if(random == 1){
+            return "paper"
+        }else if(random == 2){
+            return "scissors"
+        }
+    
     }
 
-}
-function playerChoice(){
-    let choice = prompt("please enter rock, paper, scissors").toLowerCase()
-    return choice
+    function playerChoice(){
+        let choice = prompt("please enter rock, paper, scissors").toLowerCase()
+        return choice
+    
+    }
 
-}
-function oneRound(comp, player){
-    if (comp == "rock" && player == "scissors" || comp == "paper" && player == "rock" || comp == "scissors" && player == "paper" ){
-        return `You lose! ${comp} beats ${player}`
-    } else if (player == "rock" && comp == "scissors" || player == "paper" && comp == "rock" || player == "scissors" && comp == "paper"){
-        return `You win! ${player} beats ${comp}`
-    } else if( comp == player){
+    function oneRound(comp, player){
+    
+        if (comp == "rock" && player == "scissors" || comp == "paper" && player == "rock" || comp == "scissors" && player == "paper" ){
+            
+            return `You lose! ${comp} beats ${player}`
+    
+        } else if (player == "rock" && comp == "scissors" || player == "paper" && comp == "rock" || player == "scissors" && comp == "paper"){
+            return `You win! ${player} beats ${comp}`
+        } else if( comp == player){
+            return `Its a tie! ${player} matches with ${comp}`
+            
+        }
+      
         
-         oneRound(comp, player)
-         return
+    }  
 
-    }
-  
+    for(let i = 0; i <=4; i++){
+        console.log(oneRound(getComputerChoice(), playerChoice()))
+    } 
 }
 
-// console.log(getComputerChoice())
-// console.log(playerChoice())
-console.log(oneRound(getComputerChoice(), playerChoice()))
+
+
+game()
+
+
+// console.log(game(getComputerChoice(),playerChoice() ))
